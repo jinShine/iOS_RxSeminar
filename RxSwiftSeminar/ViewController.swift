aadd559a1066fafc1b0a847ffcdce2c8ebb99e3c
jinShine <seungjin429@naver.com>
2021-07-16 02:10:21 +0900

    Subjects 추가

--- RxSwiftSeminar/ViewController.swift
+++ RxSwiftSeminar/ViewController.swift
@@ -5,27 +5,111 @@
 //  Created by buzz on 2021/07/16.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  let disposeBag = DisposeBag()
+
+  enum MyError: Error {
+    case error
   }
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
+    publishSubject()
+    behaviorSubject()
+    replaySubject()
+    asyncSubject()
+  }
 }
 
 // MARK: - Subject
 
 /*
  1. PublishSubject
+  : subscribe된 시점 이후 부터 발생한 이벤트를 전달한다.
  2. BehaviorSubject
+  : publish와 비슷하지만, 초기값을 가진다.
  3. ReplaySubject
+  : create 메서드를 생성
+  : n개의 이벤트를 저장하고 있다가 subscribe된 시점과 상관없이 버퍼의 마지막 이벤트를 전달한다.
+  : 마지막 값이 중요할때 사용
  4. AsyncSubject
+  : Complete 될때까지 이벤트는 발생되지 않고, Complete가 되면 마지막 이벤트는 발생하고 종료
  */
 
 extension ViewController {
 
+  func publishSubject() {
+    print("\n--------------[ PublishSubject ]---------------\n")
+
+    let subject = PublishSubject<String>()
+
+    subject.onNext("#1 여기선 실행 안되요~")
+
+    subject
+      .subscribe(onNext: {
+        print("Buzz", $0)
+      }).disposed(by: disposeBag)
+
+    subject.onNext("Hello")
+    subject.onError(MyError.error)
+    subject.onCompleted()
+
+    subject.onNext("Completed가 되어서 전달되지 않아요~")
+  }
+
+  func behaviorSubject() {
+    print("\n--------------[ BehaviorSubject ]---------------\n")
+
+    let subject = BehaviorSubject<String>(value: "초기값 입니다.")
+
+    subject
+      .subscribe(onNext: {
+        print("Behavior", $0)
+      }).disposed(by: disposeBag)
+
+    subject.onNext("다음 값 입니다~")
+  }
+
+  func replaySubject() {
+    print("\n--------------[ ReplaySubject ]---------------\n")
+
+    let subject = ReplaySubject<Int>.create(bufferSize: 3)
+
+    (1...10).forEach { subject.onNext($0) }
+
+    subject
+      .subscribe(onNext: {
+        print("Replay 1", $0)
+      }).disposed(by: disposeBag)
+
+    subject
+      .subscribe(onNext: {
+        print("Replay 2", $0)
+      }).disposed(by: disposeBag)
+
+    subject.onNext(11)
+  }
+
+  func asyncSubject() {
+    print("\n--------------[ AsyncSubject ]---------------\n")
+
+    let subject = AsyncSubject<Int>()
+
+    subject.onNext(1)
+
+    subject
+      .subscribe(onNext: {
+        print("Async", $0)
+      }).disposed(by: disposeBag)
+
+    subject.onNext(2)
+    subject.onNext(3)
+    subject.onCompleted()
+  }
 }
