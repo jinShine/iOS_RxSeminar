13f2639440f48bc0e458765b4e000fdd0d1e2552
jinShine <seungjin429@naver.com>
2022-01-13 02:18:28 +0900

    Login UI 구성

--- Ex_Login/Ex_Login/ViewController.swift
+++ Ex_Login/Ex_Login/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var idTextField: UITextField!
+  @IBOutlet weak var pwTextField: UITextField!
+  @IBOutlet weak var loginButton: UIButton!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
   }
-
-
 }
-
