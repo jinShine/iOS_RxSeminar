2a34f9b8e607db3af2595cca13eea806400f7152
jinShine <seungjin429@naver.com>
2022-01-19 01:39:16 +0900

    App+UI 생성

---
+++ Ex_GitHub_MVVM-C/Ex_GitHub_MVVM-C/Core/App/App+UI.swift
@@ -0,0 +1,16 @@
+//
+//  App+UI.swift
+//  Ex_GitHub_MVVM-C
+//
+//  Created by buzz on 2022/01/19.
+//
+
+import Foundation
+import UIKit
+
+extension App {
+
+  enum TableView {
+    static let rowHeight: CGFloat = 56
+  }
+}
