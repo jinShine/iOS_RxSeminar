683e123ab213d19b919127c6780eeffa19050b44
jinShine <seungjin429@naver.com>
2022-01-22 03:33:16 +0900

    Starter ObservableType extension 제거

--- Ex_GitHub_MVVM-C_Starter/Ex_GitHub_MVVM-C/Extension/Observable+Rx.swift
+++ Ex_GitHub_MVVM-C_Starter/Ex_GitHub_MVVM-C/Extension/Observable+Rx.swift
@@ -9,13 +9,4 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-extension ObservableType {
-
-  func mapToVoid() -> Observable<Void> {
-    map { _ in }
-  }
-
-  func asDriverOnErrorJustComplete() -> Driver<Element> {
-    asDriver { _ in .empty() }
-  }
-}
+extension ObservableType { }
